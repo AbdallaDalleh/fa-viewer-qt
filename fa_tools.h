65af657ee9a558cb7e60f44c82764691e8185195
Abdalla Al-Dalleh <abdalla.ahmad@sesame.org.jo>
2026-04-07 13:18:12 +0300

    Redesigned buffer_iterator as a template (buffer_iterator_base)
    The iterator supports:
      - using buffer_iterator = buffer_iterator_base<T>;
      - using const_buffer_iterator = buffer_iterator_base<const T>;

--- fa_tools.h
+++ fa_tools.h
@@ -12,52 +12,59 @@ template <typename T, size_t N>
 class buffer
 {
 public:
-    struct buffer_iterator
+    template <typename U>
+    struct buffer_iterator_base
     {
         // Iterator tags
         using iterator_category = std::random_access_iterator_tag;
         using difference_type   = std::ptrdiff_t;
-        using value_type        = T;
+        using value_type        = U;
         using pointer           = value_type*;
         using reference         = value_type&;
 
-        buffer_iterator(pointer ptr) : m_ptr(ptr) {}
+        buffer_iterator_base(pointer ptr) : m_ptr(ptr) {}
 
         reference operator*()  const { return *m_ptr; }
         pointer   operator->() const { return  m_ptr; }
 
         reference operator[] (difference_type n) const { return *(m_ptr + n); }
 
-        difference_type operator-(buffer_iterator& a) const { return m_ptr - a.m_ptr; }
-        difference_type operator+(buffer_iterator& a) const { return m_ptr + a.m_ptr; }
+        difference_type operator-(buffer_iterator_base& a) const { return m_ptr - a.m_ptr; }
+        difference_type operator+(buffer_iterator_base& a) const { return m_ptr + a.m_ptr; }
 
-        buffer_iterator operator-(difference_type n)  const { return {m_ptr - n}; }
-        buffer_iterator operator+(difference_type n)  const { return {m_ptr + n}; }
+        buffer_iterator_base operator-(difference_type n)  const { return {m_ptr - n}; }
+        buffer_iterator_base operator+(difference_type n)  const { return {m_ptr + n}; }
 
-        buffer_iterator& operator-=(difference_type n) { m_ptr -= n; return *this; }
-        buffer_iterator& operator+=(difference_type n) { m_ptr += n; return *this; }
+        buffer_iterator_base& operator-=(difference_type n) { m_ptr -= n; return *this; }
+        buffer_iterator_base& operator+=(difference_type n) { m_ptr += n; return *this; }
 
         // Prefix increment/decrement
-        buffer_iterator& operator++() { m_ptr++; return *this; }
-        buffer_iterator& operator--() { m_ptr--; return *this; }
+        buffer_iterator_base& operator++() { m_ptr++; return *this; }
+        buffer_iterator_base& operator--() { m_ptr--; return *this; }
 
         // Postfix increment/decrement
-        buffer_iterator operator++(int) { buffer_iterator temp = *this; ++(*this); return temp; }
-        buffer_iterator operator--(int) { buffer_iterator temp = *this; --(*this); return temp; }
+        buffer_iterator_base operator++(int) { buffer_iterator temp = *this; ++(*this); return temp; }
+        buffer_iterator_base operator--(int) { buffer_iterator temp = *this; --(*this); return temp; }
 
-        bool operator == (const buffer_iterator& a) const { return m_ptr == a.m_ptr; }
-        bool operator != (const buffer_iterator& a) const { return m_ptr != a.m_ptr; }
-        bool operator <  (const buffer_iterator& a) const { return m_ptr <  a.m_ptr; }
-        bool operator >  (const buffer_iterator& a) const { return m_ptr >  a.m_ptr; }
-        bool operator <= (const buffer_iterator& a) const { return m_ptr <= a.m_ptr; }
-        bool operator >= (const buffer_iterator& a) const { return m_ptr >= a.m_ptr; }
+        bool operator == (const buffer_iterator_base& a) const { return m_ptr == a.m_ptr; }
+        bool operator != (const buffer_iterator_base& a) const { return m_ptr != a.m_ptr; }
+        bool operator <  (const buffer_iterator_base& a) const { return m_ptr <  a.m_ptr; }
+        bool operator >  (const buffer_iterator_base& a) const { return m_ptr >  a.m_ptr; }
+        bool operator <= (const buffer_iterator_base& a) const { return m_ptr <= a.m_ptr; }
+        bool operator >= (const buffer_iterator_base& a) const { return m_ptr >= a.m_ptr; }
 
     private:
         pointer m_ptr;
     };
 
+    using buffer_iterator = buffer_iterator_base<T>;
+    using const_buffer_iterator = buffer_iterator_base<const T>;
+
     buffer_iterator begin() { return buffer_iterator(_data.data() + head); }
-    buffer_iterator end() { return buffer_iterator(_data.data() + head + count); }
+    buffer_iterator end()   { return buffer_iterator(_data.data() + head + count); }
+
+    const_buffer_iterator cbegin() const { return const_buffer_iterator(_data.data() + head); }
+    const_buffer_iterator cend()   const { return const_buffer_iterator(_data.data() + head + count); }
 
     explicit buffer() : head{0}, tail{0}, count{0}
     {
